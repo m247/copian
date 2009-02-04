c3927fa39ad558afcf9ccdf6c9737f5b1b0510f0
Geoff Garside <geoff@geoffgarside.co.uk>
2009-02-04 10:50:18 +0000

    Add Dell and HP collectors to the list of collectors required when the gem is loaded.

--- lib/copian.rb
+++ lib/copian.rb
@@ -3,6 +3,8 @@ require 'copian/collector/addresses_collector'
 require 'copian/collector/ports_collector'
 require 'copian/collector/generic'
 require 'copian/collector/cisco'
+require 'copian/collector/dell'
+require 'copian/collector/hp'
 
 require 'snmp'
 
