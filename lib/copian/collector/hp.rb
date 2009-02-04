dce24b9297bee76fc8dd1abc6fb56ae20973be36
Geoff Garside <geoff@geoffgarside.co.uk>
2009-02-04 13:58:20 +0000

    Add VLAN support to HP collector.
    We work around the lack of VLAN ID information from the VLAN
    collector by obtaining the VLAN ID from the ifName.

--- lib/copian/collector/hp.rb
+++ lib/copian/collector/hp.rb
@@ -1,8 +1,18 @@
 require File.expand_path(File.dirname(__FILE__) + '/hp/ports')
+require File.expand_path(File.dirname(__FILE__) + '/hp/vlans')
 
 module Copian
   module Collector
     class Hp < Generic
+      def vlans
+        load_ifnames
+
+        vlans_collector.collect do |vlan_id, vlan_index|
+          vlan_name = @ifnames[vlan_index]
+          vlan_id = vlan_name.gsub(/[^0-9]/, '').to_i
+          yield vlan_id, vlan_index, vlan_name
+        end
+      end
       def ports
         load_ifnames
 
@@ -11,6 +21,9 @@ module Copian
         end
       end
       private
+        def vlans_collector
+          @vlans_collector ||= HpVlansCollector.new(@manager)
+        end
         def ports_collector
           @ports_collector ||=
             HpPortsCollector.new(@manager)
