f64245c3a502c1fa972efa31b3fcea8add448616
Geoff Garside <geoff@geoffgarside.co.uk>
2009-02-04 13:56:28 +0000

    Add an HP VLAN collector.
    HP switches don't keep the VLAN ID in a predictable location, only
    the ifName of the ifIndex really has it in the form VLAN$ID.

---
+++ lib/copian/collector/hp/vlans.rb
@@ -0,0 +1,18 @@
+module Copian
+  module Collector
+    class HpVlansCollector < AbstractCollector
+      def collect
+        oid = SNMP::ObjectID.new('1.3.6.1.2.1.2.2.1.3')
+        @manager.walk(oid) do |r|
+          r.each do |varbind|
+            next unless varbind.value.to_i == 53  # propVirtual type
+            vlan_index = varbind.name.index(oid).to_s.to_i
+            # Not sure how to get this vlan ID, ifName sort of has it
+
+            yield nil, vlan_index
+          end
+        end
+      end
+    end
+  end
+end
