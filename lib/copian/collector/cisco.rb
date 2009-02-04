25458d7cd0fe56ea7d3cf2369e83c2341f75a0ad
Geoff Garside <geoff@geoffgarside.co.uk>
2009-02-04 10:46:35 +0000

    Push the interface name loading into the generic collector.
    The IF-Names SNMP OID is standardised and so should be the same
    across all SNMP devices with interfaces.

--- lib/copian/collector/cisco.rb
+++ lib/copian/collector/cisco.rb
@@ -25,21 +25,6 @@ module Copian
         end
       end
       private
-        def load_ifnames
-          return if @loaded_ifnames
-          oid = SNMP::ObjectId.new('1.3.6.1.2.1.31.1.1.1.1')
-          @manager.walk(oid) do |r|
-            r.each do |varbind|
-              append_ifname(varbind.name.index(oid).to_s.to_i,
-                varbind.value.to_s)
-            end
-          end
-          @loaded_ifnames = true
-        end
-        def append_ifname(ifindex, ifname)
-          @ifnames ||= Hash.new
-          @ifnames[ifindex] = ifname
-        end
         def vlans_collector
           @vlans_collector ||= CiscoVlansCollector.new(@manager)
         end
--- lib/copian/collector/generic.rb
+++ lib/copian/collector/generic.rb
@@ -8,6 +8,22 @@ module Copian
         @manager = SNMP::Manager.new(:Host => ip_addr,
           :Community => community, :Version => version)
       end
+      protected
+        def load_ifnames
+          return if @loaded_ifnames
+          oid = SNMP::ObjectId.new('1.3.6.1.2.1.31.1.1.1.1')
+          @manager.walk(oid) do |r|
+            r.each do |varbind|
+              append_ifname(varbind.name.index(oid).to_s.to_i,
+                varbind.value.to_s)
+            end
+          end
+          @loaded_ifnames = true
+        end
+        def append_ifname(ifindex, ifname)
+          @ifnames ||= Hash.new
+          @ifnames[ifindex] = ifname
+        end
     end
   end
 end
